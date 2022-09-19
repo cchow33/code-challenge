8aee597840c41f8accf02f801e4f57e1fc26f0ea
cchow33 <109078860+cchow33@users.noreply.github.com>
2022-09-19 17:02:03 -0400

    Create submission.js

---
+++ submission.js
@@ -0,0 +1,29 @@
+Problem 1:
+const findSum = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+Problem 2:
+const findFrequency = function(array) {
+  // your code here - don't forget to return an object!
+};
+
+Problem 3:
+const isPalindrome = function(str) {
+  // your code here - don't forget to return a boolean!
+};
+
+Problem 4:
+const largestPair = function(array) {
+  // your code here - don't forget to return a number!
+};
+
+Problem 5:
+const removeParenth = function(str) {
+  // your code here - don't forget to return a string!
+};
+
+Problem 6:
+const scoreScrabble = function(str) {
+  // your code here - don't forget to return a number!
+};
