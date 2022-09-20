6920b49ec4e1f8246396ef3929341f0fd694fbb1
cchow33 <109078860+cchow33@users.noreply.github.com>
2022-09-20 10:33:47 -0400

    Problem 6

--- submission.js
+++ submission.js
@@ -1,3 +1,4 @@
+// Problem 1
 const findSum = function(array) {
     let sum = 0;
     let average = 0;
@@ -10,20 +11,21 @@ const findSum = function(array) {
 };
 
 
+// Problem 2
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
 };
 
 
 
-
+// Problem 3
 const isPalindrome = function(str) {
     var reversedStr = str.toLowerCase().split('').reverse().join('') //=
     return str === reversedStr ? true : false; //=
 };
 
 
-
+// Problem 4
 const largestPair = function(array){
     let product = 0;
     let newArr = []
@@ -36,11 +38,46 @@ const largestPair = function(array){
 };
 
 
-
+// Problem 5
 const removeParenth = function(str) {
   // your code here - don't forget to return a string!
 };
 
+// Problem 6
 const scoreScrabble = function(str) {
-  // your code here - don't forget to return a number!
+  const scoreScrabble = function(str) {
+    const points = {
+        a: 1,
+        e: 1,
+        i: 1,
+        o: 1,
+        u: 1,
+        l: 1,
+        n: 1,
+        r: 1,
+        s: 1,
+        t: 1,
+        d: 2,
+        g: 2,
+        b: 3,
+        c: 3,
+        m: 3,
+        p: 3,
+        f: 4,
+        h: 4,
+        v: 4,
+        w: 4,
+        y: 4,
+        k: 5,
+        j: 8,
+        x: 8,
+        q: 10,
+        z: 10,
+    };
+
+    let totalPoints = 0;
+    for (let i = 0; i < str.length; i++){
+        totalPoints += points[str[i]]
+    }
+    return totalPoints;
 };
